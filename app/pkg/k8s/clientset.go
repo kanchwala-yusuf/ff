b708415fb1c02494fb55280ad5851056858c1039
Yusuf Kanchwala <yusuf.kanchwala@macrometa.co>
2019-10-01 12:47:44 +0530

    Vendoring Update #2

--- app/pkg/k8s/clientset.go
+++ app/pkg/k8s/clientset.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"k8s.io/client-go/kubernetes"
-	//_ "k8s.io/client-go/plugin/pkg/client/auth"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
